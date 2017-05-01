8bec25893510d8ebae3770edab8bfefc9cebb3e5
Deac Karns <deac@sfp.net>
2017-05-01 16:08:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Install
+
+```
+sudo git clone https://github.com/peledies/devsite-init.git /opt/devsite
+```
+
+### Create Symbolic Link to devsite-init
+```
+ln -s /opt/devsite-init/devsite.sh /usr/local/bin/devsite
+```
+
+## Updating
+
+```
+cd /opt/devsite-init && sudo git pull && cd -
+```
---
+++ devsite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+red=$(tput setaf 1)
+default=$(tput sgr0)
+
+if [ -z "$1" ];then
+  echo "${red}You must specify a directory to create.${default}"
+else
+  echo "${cyan}Creating new development project in ${magenta}`pwd`/$1 ${default}"
+  git clone https://github.com/peledies/devsite.git $1 2>&1
+
+  cd $1
+
+  echo "${cyan}You will need to set the origin.${default}"
+  git remote remove origin
+fi
